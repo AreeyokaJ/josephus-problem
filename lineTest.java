509326197ce5d882fb9dccfbb804eb21abf49ab4
AreeyokaJ <97417918+AreeyokaJ@users.noreply.github.com>
2024-04-14 23:37:28 -0400

    Update lineTest.java

--- lineTest.java
+++ lineTest.java
@@ -7,7 +7,7 @@ public class lineTest{
 
 
         try{
-            //instantiatle line object which will run through course of program
+            //instantiate line object which will run through course of program
             Line line1 = new Line(); 
         }
 
@@ -16,7 +16,7 @@ public class lineTest{
         }
 
         catch(InputMismatchException e){
-            System.out.println("Please be sure to enter only intgers.");
+            System.out.println("Please be sure to enter only integers.");
         }
         
     }
